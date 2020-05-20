c1726f4ae9192f72055dcbea87fc0ef85a2ca5a4
luciven <luciven01@gmail.com>
2020-05-21 01:15:00 +0530

    Added username in chat and room isolation

--- game/admin.py
+++ game/admin.py
@@ -9,5 +9,10 @@ class RoomAdmin(admin.ModelAdmin):
     fields = ('host_user', 'roomname', 'no_of_players', 'no_of_rounds')
     list_display = ('host_user', 'roomname', 'no_of_players', 'no_of_rounds')
 
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('author', 'content', 'timestamp')
+    list_display = ('author', 'content', 'timestamp')
+
 admin.site.register(GuestUser, GuestUserAdmin)
 admin.site.register(Room, RoomAdmin)
+admin.site.register(Message, MessageAdmin)
--- game/models.py
+++ game/models.py
@@ -10,3 +10,15 @@ class Room(models.Model):
 	roomname=models.CharField(max_length=50)
 	no_of_players=models.CharField(max_length=50)
 	no_of_rounds=models.CharField(max_length=50)
+
+class Message(models.Model):
+	author = models.ForeignKey(GuestUser, related_name="author_messages", on_delete=models.CASCADE)
+	room = models.ForeignKey(Room, on_delete=models.CASCADE)
+	content = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.author.username
+
+	def last_50_messages(roomId):
+		return Message.objects.order_by('timestamp').filter('room'.roomname==roomId)[:50]
